a093a7a9de64314779e71e8d49a0e33e70f3352a
KrisBhatt <krishnadev@unifytech.com>
2023-06-26 19:00:28 +0530

    main Initial Commit with amplify login/signup page

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifynextjs108e584c93": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "amplifynextjs108e584c93GroupRole": "string"
+    }
+  }
+}
